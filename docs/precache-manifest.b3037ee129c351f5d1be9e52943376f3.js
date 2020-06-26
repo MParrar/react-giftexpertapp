49da146cbbc21768cc8ae924cedec4984935fa4b
Matias Parra <parramatias96@gmail.com>
2020-06-26 08:51:09 -0400

    Docs

---
+++ docs/precache-manifest.b3037ee129c351f5d1be9e52943376f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d881430757c7828a306f03449b117fc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7871a44959184f9c09c2",
+    "url": "/static/css/main.fde3c47c.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7871a44959184f9c09c2",
+    "url": "/static/js/main.1d3883cf.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
